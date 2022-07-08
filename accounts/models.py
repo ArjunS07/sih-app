d97ac9ad738b11e11654dd43f34089d59f75f26a
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-07-08 11:51:37 +0530

    Update models

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,8 @@ import django.contrib.postgres.fields
 
 from multiselectfield import MultiSelectField
 
-from .choices import LANGUAGE_MEDIUM_CHOICES, GRADE_CHOICES, CITY_CHOICES, BOARD_CHOICES, SUBJECT_CHOICES
+from api.models import Tutorship
+from ..api.choices import LANGUAGE_MEDIUM_CHOICES, GRADE_CHOICES, CITY_CHOICES, BOARD_CHOICES, SUBJECT_CHOICES
 
 class User(AbstractUser):
     # Must have the same email as their zoom account
@@ -14,20 +15,31 @@ class User(AbstractUser):
 
 
 class PlatformUser(User):
-    name_id = models.TextField(primary_key=True, editable=False)
+    name_id = models.CharField(primary_key=True, editable=False, max_length=128)
     language_medium = MultiSelectField(choices=LANGUAGE_MEDIUM_CHOICES, null=True, blank=True)
     city = models.CharField(choices=CITY_CHOICES, null=True, blank=True, max_length=32)
 
-    def save(self, *args, **kwargs):
-        # Object hasn't been created in the DB yet
-        if not self.pk:
-            # Make a UUID from the username
-            self.name_id = uuid.uuid5(uuid.NAMESPACE_DNS, self.username)
-    
+    def __init__(self, name_id, language_medium, city):
+        self.name_id = uuid.uuid5(uuid.NAMESPACE_DNS, self.username)
+        self.language_medium = language_medium
+        self.city = city    
 class School(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.TextField(default=None, null=True)
 
+    @property
+    def school_students(self) -> int:
+        return (Student.objects.filter(school=self, is_active=True))
+    
+    @property
+    def name_id(self) -> str:
+        return self.account.name_id
+    
+    @property
+    def email(self) -> str:
+        return self.account.email
+    
+    
 class Student(models.Model):
     account = models.OneToOneField(PlatformUser, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE) 
@@ -37,9 +49,17 @@ class Student(models.Model):
     grade = models.CharField(max_length=2, choices=GRADE_CHOICES)
     subjects = MultiSelectField(choices=SUBJECT_CHOICES, max_length=8)
 
+    @property
+    def is_active(self) -> bool:
+        return self.account.is_active
+
 class Tutor(models.Model):
     account = models.OneToOneField(PlatformUser, on_delete=models.CASCADE)
     
     # Personal info
     subjects = MultiSelectField(choices=SUBJECT_CHOICES, max_length=8)
     grade_range = MultiSelectField(choices=GRADE_CHOICES, max_length=12)
+
+    @property
+    def get_tutor_active_mentorships(self) -> int:
+        return Tutorship.objects.get(tutor=self)
--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,12 @@
-"""
+from unittest.util import _MAX_LENGTH
 from rest_framework import fields, serializers
+
+
+from .models import Tutorship, Message, Meeting
+from accounts.models import PlatformUser, School, Student, Tutor
+
+from .choices import LANGUAGE_MEDIUM_CHOICES, GRADE_CHOICES, CITY_CHOICES, BOARD_CHOICES, SUBJECT_CHOICES
+"""
 
 from myapp.models import MY_CHOICES, MY_CHOICES2
 
@@ -8,3 +15,32 @@ class MyModelSerializer(serializers.HyperlinkedModelSerializer):
     my_field2 = fields.MultipleChoiceField(choices=MY_CHOICES2)
 
 """
+
+class AccountSerializer(serializers.Serializer):
+    pass
+
+class PlatformUserSerializer(serializers.Serializer):
+    name_id = serializers.CharField(max_length=128)
+    language_medium = serializers.MultipleChoiceField(choices=LANGUAGE_MEDIUM_CHOICES)
+    city = serializers.CharField(choices=CITY_CHOICES, max_length=32)
+
+class SchoolSerializer(serializers.Serializer):
+    account
+
+class StudentSerializer(serializers.Serializer):
+    subjects = fields.MultipleChoiceField(choices=SUBJECT_CHOICES)
+
+    pass
+
+class TutorSerializer(serializers.Serializer):
+    pass
+
+class TutorshipSerializer(serializers.Serializer):
+
+    pass
+
+class MessageSerializer(serializers.Serializer):
+    pass
+
+class MeetingSerializer(serializers.Serializer):
+    pass
