ca623f23ef8214eede1c778804c856a185ac1d67
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-07-11 18:12:11 +0530

    Fix conflicts

--- api/models.py
+++ api/models.py
@@ -2,7 +2,52 @@ import uuid
 
 from django.db import models
 
-from accounts.models import School, Student, Tutor
+from multiselectfield import MultiSelectField
+
+from accounts.models import PlatformUser
+from .choices import LANGUAGE_MEDIUM_CHOICES, GRADE_CHOICES, CITY_CHOICES, BOARD_CHOICES, SUBJECT_CHOICES
+
+class School(models.Model):
+    account = models.OneToOneField(PlatformUser, on_delete=models.CASCADE)
+    name = models.TextField(default=None, null=True)
+
+    @property
+    def school_students(self) -> int:
+        return (Student.objects.filter(school=self, is_active=True))
+    
+    @property
+    def name_id(self) -> str:
+        return self.account.name_id
+    
+    @property
+    def email(self) -> str:
+        return self.account.email
+    
+    
+
+class Student(models.Model):
+    account = models.OneToOneField(PlatformUser, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE) 
+
+    # Personal info
+    board = models.CharField(max_length=16, choices=BOARD_CHOICES, null=True, blank=True)
+    grade = models.CharField(max_length=2, choices=GRADE_CHOICES)
+    subjects = MultiSelectField(choices=SUBJECT_CHOICES, max_length=8)
+
+    @property
+    def is_active(self) -> bool:
+        return self.account.is_active
+
+class Tutor(models.Model):
+    account = models.OneToOneField(PlatformUser, on_delete=models.CASCADE)
+    
+    # Personal info
+    subjects = MultiSelectField(choices=SUBJECT_CHOICES, max_length=8)
+    grade_range = MultiSelectField(choices=GRADE_CHOICES, max_length=12)
+
+    @property
+    def get_tutor_active_mentorships(self) -> int:
+        return Tutorship.objects.get(tutor=self)
 
 class Tutorship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
