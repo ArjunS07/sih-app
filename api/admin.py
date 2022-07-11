d347af8794e37ffcfd10339af4104c82f055ec9b
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-07-11 19:46:37 +0530

    Update DB

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from .models import School, Student, Tutor
 from .models import Tutorship, Message, Meeting
+
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(School, SchoolAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Student, StudentAdmin)
+
+class TutorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Tutor, TutorAdmin)
 
 # Register your models here.
 class TutorshipAdmin(admin.ModelAdmin):
