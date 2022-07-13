78214a90672911f97a0161dbed5eccb7e7777d5a
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-07-13 19:02:44 +0530

    Clean up API

--- api/views.py
+++ api/views.py
@@ -16,64 +16,45 @@ from .choices import SUBJECT_CHOICES, LANGUAGE_MEDIUM_CHOICES, GRADE_CHOICES, BO
 
 class TutorList(APIView):
     def get(self, request, format=None):
-        if request.method == 'GET':
-            data = request.query_params
-            print(request)
-            q = []
-            if 'languages' in data:
-                languages = data['languages'].split(',')
-                for language in languages:
-                    q.append(Q(languages__contains=language))
-            if 'boards' in data:
-                boards = data['boards'].split(',')
-                for board in boards:
-                    q.append(Q(boards__contains=board))
-            if 'subjects' in data: 
-                subjects = data['subjects'].split(',')
-                for subject in subjects:
-                    print(subject)
-                    q.append(Q(subjects__contains=subject))
-            if 'grades' in data: 
-                grades = data['grades'].split(',')
-                for grade in grades:
-                    q.append(Q(grades__contains=grade))
+        data = request.query_params
+        print(request)
+        q = []
+        if 'languages' in data:
+            languages = data['languages'].split(',')
+            for language in languages:
+                q.append(Q(languages__contains=language))
+        if 'boards' in data:
+            boards = data['boards'].split(',')
+            for board in boards:
+                q.append(Q(boards__contains=board))
+        if 'subjects' in data: 
+            subjects = data['subjects'].split(',')
+            for subject in subjects:
+                print(subject)
+                q.append(Q(subjects__contains=subject))
+        if 'grades' in data: 
+            grades = data['grades'].split(',')
+            for grade in grades:
+                q.append(Q(grades__contains=grade))
 
-            if len(q) > 0:
-                matching_tutors = Tutor.objects.filter(
-                    reduce(operator.and_, q)
-                )
-            else:
-                matching_tutors = Tutor.objects.all()
+        if len(q) > 0:
+            matching_tutors = Tutor.objects.filter(
+                reduce(operator.and_, q)
+            )
+        else:
+            matching_tutors = Tutor.objects.all()
 
-            print(matching_tutors)
+        print(matching_tutors)
 
-            serialized_tutors = serializers.TutorSerializer(matching_tutors, many=True)
-            res = JSONRenderer().render(serialized_tutors.data)
-            return HttpResponse(res, status=status.HTTP_200_OK)
+        serialized_tutors = serializers.TutorSerializer(matching_tutors, many=True)
+        res = JSONRenderer().render(serialized_tutors.data)
+        return HttpResponse(res, status=status.HTTP_200_OK)
 
-"""
-try:
-    languages = data['languages'].split(',')
-    boards = data['boards'].split(',')
-    subjects = data['subjects']
+class Tutorship(APIView):
+    def get(self, request, format=None):
+        data = request.query_
 
-    grade_ints = data['grade_int']
-except Exception as e:
-    print(e)
-    return HttpResponse('Missing parameters', status=status.HTTP_400_BAD_REQUEST)
 
-grade_codes = [map_int_to_grade(grade) for grade in grade_ints]
-
-matching_tutors = Tutor.objects.filter(
-    languages=languages,
-    boards=boards,
-    subjects=subjects,
-    grades=grade_codes,
-)
-
-return HttpResponse(matching_tutors, status=status.HTTP_200_OK)
-
-"""
 def JoinSchool(APIView):
     def post(self, request, format=None):
         data = request.POST
