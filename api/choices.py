d24dd9807c925e7f96715e47c18f2755e4706435
Arjun Sharma <arjun.sharma07@outlook.com>
2022-07-22 22:36:36 +0530

    Update choices

--- api/choices.py
+++ api/choices.py
@@ -3,82 +3,36 @@ from csv import reader
 import os
 
 base_path = pathlib.Path().resolve()
+def all_choices(choice_set):
+    ids = [choice[0] for choice in choice_set]
+    return ids
 
 LANGUAGE_MEDIUM_CHOICES = []
 language_path = os.path.join(base_path, 'api', 'resources', 'languages.csv')
-
 with open(language_path, 'r') as f:
     csv_reader = reader(f)
     LANGUAGE_MEDIUM_CHOICES = tuple(csv_reader)
 
 CITY_CHOICES = []
 city_path = os.path.join(base_path, 'api', 'resources', 'cities.csv')
-
 with open(city_path, 'r') as f:
     csv_reader = reader(f)
     CITY_CHOICES = list(map(tuple, csv_reader))
 
-def all_choices(choice_set):
-    ids = [choice[0] for choice in choice_set]
-    return ids
+GRADE_CHOICES = []
+grade_path = os.path.join(base_path, 'api', 'resources', 'grades.csv')
+with open(grade_path, 'r') as f:
+    csv_reader = reader(f)
+    GRADE_CHOICES = list(map(tuple, csv_reader))
 
-GRADE_CHOICES = [
-    ('NS', 'Nursery'),
-    ('KG', 'Kindergarten'),
-    ('1G', 'First grade'),
-    ('2G', 'Second grade'),
-    ('3G', 'Third grade'),
-    ('4G', 'Fourth grade'),
-    ('5G', 'Fifth grade'),
-    ('6G', 'SIXTH grade'),
-    ('7G', 'Seventh grade'),
-    ('8G', 'Eighth grade'),
-    ('9G', 'Ninth grade'),
-    ('10G', 'Tenth grade'),
-    ('11G', 'Eleventh grade'),
-    ('12G', 'Twelfth grade'),
-]
+BOARD_CHOICES = []
+subject_path = os.path.join(base_path, 'api', 'resources', 'boards.csv')
+with open(subject_path, 'r') as f:
+    csv_reader = reader(f)
+    BOARD_CHOICES = list(map(tuple, csv_reader))
 
-
-BOARD_CHOICES = [
-    ('CBSE', 'CBSE'),
-    ('STATE', 'State board'),
-    ('ICSE', 'ICSE'),
-    ('ISC', 'ISC'),
-    ('IGCSE', 'IGCSE'),
-    ('IB', 'IB'),
-    ('OTHER', 'Other'),
-]
-
-SUBJECT_CHOICES = [
-    ('MATH', 'Mathematics'),
-
-    ('ENGLISH', 'English'),
-    ('HIN', 'Hindi'),
-    ('SANSK', 'Sanskrit'),
-    ('SP', 'Spanish'),
-    ('FR', 'French'),
-    ('GR', 'German'),
-
-    ('EVS', 'Environmental science (EVS)'),
-    ('SCIENCE', 'Science'),
-    ('BIO', 'Biology'),
-    ('PHY', 'Physics'),
-    ('CHEM', 'Chemistry'),
-    ('CS', 'Computer science'),
-    ('PSYCH', 'PSYCHOLOGY'),
-
-    ('SST', 'Social studies / Social science'),
-    ('HIST', 'History'),
-    ('CIV', 'Civics'),
-    ('GEO', 'Geography'),
-    ('ECO', 'Economics'),
-    ('COM', 'Commerce'),
-    ('ACC', 'Accounting'),
-    ('SOC', 'Sociology'),
-
-    ('HOM', 'Home science'),
-    ('ART', 'Art'),
-]
-
-
+SUBJECT_CHOICES = []
+subject_path = os.path.join(base_path, 'api', 'resources', 'subjects.csv')
+with open(subject_path, 'r') as f:
+    csv_reader = reader(f)
+    SUBJECT_CHOICES = list(map(tuple, csv_reader))
