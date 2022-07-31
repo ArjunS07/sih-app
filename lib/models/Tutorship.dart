6f5407c5ff50e2fea02410eb1f29886236dd4532
Arjun Sharma <arjun.sharma07@outlook.com>
2022-07-31 13:07:46 +0530

    Add support for time

--- lib/models/Tutorship.dart
+++ lib/models/Tutorship.dart
@@ -1,17 +1,18 @@
-import 'tutor.dart';
-import 'student.dart';
+import 'package:sih_app/utils/extensions/date_time_extension.dart';
+
 
 class Tutorship {
   final int id;
   final String studentUuid;
   final String tutorUuid;
   String status = 'PNDG';
+  final DateTime createdTime; // iso 8601 native datetime
 
   Tutorship(
       {required this.id,
       required this.studentUuid,
       required this.tutorUuid,
-      required this.status});
+      required this.status, required this.createdTime});
 
   factory Tutorship.fromJson(Map<String, dynamic> json) {
     return Tutorship(
@@ -19,6 +20,12 @@ class Tutorship {
       studentUuid: json['student']['uuid'],
       tutorUuid: json['tutor']['uuid'],
       status: json['status'],
+      createdTime: DateTime.parse(json['created'])
     );
   }
+
+  String get relativeTimeSinceCreated {
+    return createdTime.timeAgo(numericDates: false);
+  }
+
 }
