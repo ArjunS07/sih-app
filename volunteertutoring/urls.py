2bfed37ec8158b7e2d74e91fbf2a51d30c8e586b
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-01 16:49:13 +0530

    Remove stuff for heroku

--- volunteertutoring/urls.py
+++ volunteertutoring/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('app/', include('core.urls')),
     path('accounts/', include('accounts.urls')),
