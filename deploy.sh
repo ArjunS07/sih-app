4845722608ad5301d1b9a33733f7d96761b2e03a
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-01 16:48:45 +0530

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo yes | python manage.py collectstatic
 git add .
 git commit -m "update static files"
-git push heroku master
+git push heroku main
 heroku run python3 manage.py makemigrations
 heroku run python3 manage.py migrate
 heroku open
