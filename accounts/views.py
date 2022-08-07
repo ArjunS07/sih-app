5e1c9818907ccde6bb980fa88b8b7365a91349b6
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-07 23:27:19 +0530

    Add accounts patch

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -14,6 +15,7 @@ from rest_framework import status
 
 from . import serializers
 
+
 class CustomAuthToken(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
@@ -27,11 +29,13 @@ class CustomAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
-from django.contrib.auth import get_user_model
+
+
 User = get_user_model()
 
+
 class UserView(APIView):
-     def get(self, request, format=None):
+    def get(self, request, format=None):
         account_id = request.query_params.get('id', None)
         try:
             account = User.objects.get(id=account_id)
@@ -40,3 +44,23 @@ class UserView(APIView):
         serialized_account = serializers.UserModelSerializer(account)
         res = JSONRenderer().render(serialized_account.data)
         return HttpResponse(res, content_type='application/json', status=status.HTTP_200_OK)
+
+    def patch(self, request, *args, **kwargs):
+        data = request.data
+        print(request)
+        print(data)
+        account_id = data.get('id', None)
+        if not account_id:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+        try:
+            account = User.objects.get(id=account_id)
+        except:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        serializer = serializers.UserModelSerializer(
+            account, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = JSONRenderer().render(serializer.data)
+            return HttpResponse(res, content_type='application/json', status=status.HTTP_200_OK)
+        else:
+            return HttpResponse(serializer.errors, content_type='application/json', status=status.HTTP_400_BAD_REQUEST)
