63222e692b90279b6a24076600e68dc930ac756f
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-10 18:19:49 +0530

    Add sentry

--- volunteertutoring/settings.py
+++ volunteertutoring/settings.py
@@ -201,3 +201,24 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGIN_URL = 'app/login'
 LOGOUT_REDIRECT_URL = '/'
+
+import sentry_sdk
+
+if not DEBUG:
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn="https://820c221a08ec4d9f84743ba883305dfe@o879237.ingest.sentry.io/6638036",
+        integrations=[
+            DjangoIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
