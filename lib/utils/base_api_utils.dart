890a1acb4e206a66805a6891782fc8ef61b483d3
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-10 19:01:58 +0530

    Update API calls

--- lib/utils/base_api_utils.dart
+++ lib/utils/base_api_utils.dart
@@ -1,4 +1,5 @@
 import 'package:uuid/uuid.dart';
 
-final String ROOT_URL = 'http://localhost:8000';
+// final String ROOT_URL = 'http://localhost:8000';
+final String ROOT_URL = 'https://edudaan.herokuapp.com';
 Uuid uuid = const Uuid();
