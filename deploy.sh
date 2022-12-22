e46a400b8246e23e9d79811c0226f38608838777
ArjunS07 <arjun.sharma07@outlook.com>
2022-12-22 19:58:47 +0530

    Remove old files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-echo yes | python manage.py collectstatic
-git add .
-git commit -m "update static files"
-git push heroku main
-heroku run python3 manage.py makemigrations
-heroku run python3 manage.py migrate
-heroku open
-git push origin main
